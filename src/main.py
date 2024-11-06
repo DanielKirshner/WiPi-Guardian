814b3d19b4e6f5967f2099b38308caaa38cb604c
DanielKirshner <danielkirshner7878@gmail.com>
2024-11-06 19:23:49 +0200

    Telegram bot is now a RAII class

--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
+from telegram_bot.telegram_bot import TelegramBot
 from common import logger
 import logging
+import os
 
 def main():
     logger.init()
     logging.info("Main started!")
+    
+    try:
+        BOT_TOKEN_FILE_PATH = os.path.join("config", "bot_token.txt")
+        
+        with open(BOT_TOKEN_FILE_PATH, "r") as f:
+            TELEGRAM_API_TOKEN = f.read().strip()
+    except FileNotFoundError:
+        logging.error(f"{BOT_TOKEN_FILE_PATH} file not found.")
+        return
+    
+    # Create and run the bot
+    with TelegramBot(TELEGRAM_API_TOKEN) as bot:
+        bot.run_bot()
 
 
 if __name__ == "__main__":
--- src/telegram_bot/telegram_bot.py
+++ src/telegram_bot/telegram_bot.py
@@ -1,18 +1,22 @@
 from telegram.ext import Application, CommandHandler
+import logging
 
-# Read telegram api token
-with open("bot_token.txt", "r") as f:
-    TELEGRAM_API_TOKEN = f.read().strip()
+class TelegramBot:
+    def __init__(self, token):
+        self.application = Application.builder().token(token).build()
+        self._register_all_handlers()
 
-# This function triggers when someone starts the bot with the `/start` command
-async def start(update, context):
-    await update.message.reply_text("Hey!\nWelcome to Pi-Fi guardian Telegram bot!")
+    def _register_all_handlers(self):
+        self.application.add_handler(CommandHandler("start", self.start))
 
-# Create the application instance
-application = Application.builder().token(TELEGRAM_API_TOKEN).build()
+    async def start(self, update, context):
+        await update.message.reply_text("Hey!\nWelcome to Pi-Fi guardian Telegram bot!")
 
-# Add a handler for the `/start` command
-application.add_handler(CommandHandler("start", start))
+    def run_bot(self):
+        self.application.run_polling()
 
-# Run the bot
-application.run_polling()
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        logging.info("Telegram bot is shutting down.")
